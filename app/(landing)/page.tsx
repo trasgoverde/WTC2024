a93bb24350250741c8688d4338c43d84756fde96
Ignacio <ign.souto@gmail.com>
2024-01-02 16:29:47 +0100

    First Commit- Magic.link Implementation

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+import { Button } from "@/components/ui/button";
+const LandingPage = () => {
+    return (
+        <div>
+            Landing Page (Unprotected)
+        </div>   
+    );
+}
+export default LandingPage;
